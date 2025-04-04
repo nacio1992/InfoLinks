6e841b96f9bfd384cd909378b1619520a4bbebf7
inacio <souzainacio440@gmail.com>
2025-04-04 20:51:25 -0300

    primeiro commit

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { View,Text} from "react-native";
+
+
+export default function Index () {
+    return(
+        <View>
+            <Text> eu sou o milior!!</Text>
+        </View>
+    );
+}
